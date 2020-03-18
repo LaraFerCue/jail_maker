365881e7925537b31cf054a86302ab7351421e98
Lara Fernandez Cueto <l.fernandez-cueto@bally-wulff.de>
2020-03-18 09:20:27 +0100

    Add tests and fix cloning a dataset

--- Mk/bsd.jails.zfs.mk
+++ Mk/bsd.jails.zfs.mk
@@ -12,11 +12,22 @@
 __bsd.jails.zfs.mk__:
 
 ZPOOL?=		zroot
+DATE_FORMAT?=	%Y-%m-%d_%H:%M:%S
+BASE_JAIL_DATASET=	${ZPOOL}${JAIL_BASEPATH}/${JAIL_VERSION}
 
-${JAIL_BASEPATH} \
-	${JAIL_BASEPATH}/${JAIL_VERSION} \
-	${JAIL_BASEPATH}/${JAIL_NAME}: .PHONY
+${JAIL_BASEPATH}/${JAIL_VERSION}: ${JAIL_BASEPATH} .PHONY
+${JAIL_BASEPATH} ${JAIL_BASEPATH}/${JAIL_VERSION}: .PHONY
 	zfs list ${ZPOOL}${@} > /dev/null 2> /dev/null || \
 		zfs create ${ZPOOL}${@}
+
+base_jail_snapshot: ${JAIL_BASEPATH}/${JAIL_VERSION}/.snapshot .PHONY
+
+${JAIL_BASEPATH}/${JAIL_VERSION}/.snapshot: .META
+	echo ${DATE_FORMAT:gmtime} > ${@}
+	zfs snapshot ${BASE_JAIL_DATASET}@`cat ${@}`
+
+${JAIL_BASEPATH}/${JAIL_NAME}: base_jail_snapshot .PHONY
+	zfs list ${ZPOOL}${@} || \
+		zfs clone ${BASE_JAIL_DATASET}@`cat ${JAIL_BASEPATH}/${JAIL_VERSION}/.snapshot` ${ZPOOL}${@}
 
 .endif # ! target(__bsd.jails.zfs.mk__)
